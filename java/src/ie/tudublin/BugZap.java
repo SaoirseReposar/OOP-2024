f2467c4dd04e9c7932f2222b9dc67cc8ac5bcbe2
SaoirseReposar <C22448686@mytudublin.ie>
2024-02-01 19:49:40 +0000

    bug

--- java/src/ie/tudublin/BugZap.java
+++ java/src/ie/tudublin/BugZap.java
@@ -3,11 +3,18 @@ package ie.tudublin;
 import processing.core.PApplet;
 
 public class BugZap extends PApplet {
+
+	float playerX = 50.0f; 
+    float playerY = 50.0f; 
+    float playerWidth = 30.0f;
+
 	public void settings() {
 		size(1000, 1000);
 	}
 
 	public void setup() {
+		playerX = width / 2;
+        playerY = height / 2;
 		
 	}
 
@@ -31,5 +38,15 @@ public class BugZap extends PApplet {
 	int gameMode = 0;
 
 	public void draw() {
+		background(255); // White background
+        drawPlayer(playerX, playerY, playerWidth);
 	}
+
+	public void drawPlayer(float x, float y, float w) {
+        float h = w * 0.5f; // Calculate half of the width for the height
+        stroke(0); // Set stroke color to black
+        line(x - w / 2, y, x + w / 2, y); // Draw horizontal line
+        line(x, y - h / 2, x, y + h / 2);
+	}
+
 }
