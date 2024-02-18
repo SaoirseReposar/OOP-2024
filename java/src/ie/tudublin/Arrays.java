3fa18311c6bbaa67f8f8a4b83162f7fccc30eb7e
SaoirseReposar <C22448686@mytudublin.ie>
2024-02-18 20:16:10 +0000

    Rainbow chart

--- java/src/ie/tudublin/Arrays.java
+++ java/src/ie/tudublin/Arrays.java
@@ -7,15 +7,15 @@ import processing.core.PApplet;
 //Week 4 lab, part 2:
 
 public class Arrays extends PApplet {
-    String[] months = {"Jan", "Feb", "March", "April", "May", "June", "July", "Aug", "Sep", "Oct", "Nov", "December"};
-    int[] rainfall = {41, 35, 57, 24, 37, 50, 80, 48, 105, 30, 102, 60}; // Sample rainfall values for each month
+    String[] months = {"Jan", "Feb", "March", "April", "May", "June", "July", "Aug", "Sep", "Oct", "Nov", "Dec"};
+    int[] rainfall = {41, 35, 57, 24, 37, 50, 80, 48, 105, 30, 102, 60}; 
 
     public void settings() {
         size(800, 400);
     }
 
     public void setup() {
-        colorMode(HSB); // Use HSB color mode for easier color control
+        colorMode(HSB); 
         background(0);
         drawRainfallChart();
     }
@@ -24,8 +24,8 @@ public class Arrays extends PApplet {
     }
 
     public void drawRainfallChart() {
-        float barWidth = (width - 150) / (float) (months.length + 1); // Adjusted width for bars
-        float maxRainfall = 120; // Maximum rainfall value
+        float barWidth = (width - 150) / (float) (months.length + 1); 
+        float maxRainfall = 120; 
 
         textAlign(RIGHT, CENTER);
         for (int level = 0; level <= maxRainfall; level += 10) {
@@ -43,12 +43,10 @@ public class Arrays extends PApplet {
         line(100, 0, 100, height);
         line(100, height - 20, width, height - 20);
 
-        // Draw the bars for each month with different colors
         for (int i = 0; i < months.length; i++) {
             float x = map(i + 1, 1, months.length + 1, 140, width - 30);
             float barHeight = map(rainfall[i], 0, maxRainfall, 0, height - 20);
-
-            // Assign colors based on the month
+            
             int barColor = color(map(i, 0, months.length, 0, 360), 255, 255);
             fill(barColor);
             rect(x - barWidth / 2, height - barHeight - 20, barWidth, barHeight);
